4142ba2cf9050a2a0179c1d7cf2357780ac586d1
Swavek <swaveklorenc@yahoo.com>
2021-02-09 09:55:00 -0600

    Blank out birth date in user profile editor if we are coming from registration link where the date has been set to today.

--- client/src/app/profile/profile-edit/profile-edit.component.ts
+++ client/src/app/profile/profile-edit/profile-edit.component.ts
@@ -1,14 +1,15 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange, SimpleChanges} from '@angular/core';
 import {Profile} from '../profile';
 import {StatesList} from '../../shared/states/states-list';
 import {CountriesList} from '../../shared/countries-list';
+import {DateUtils} from '../../shared/date-utils';
 
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.component.html',
   styleUrls: ['./profile-edit.component.css']
 })
-export class ProfileEditComponent implements OnInit {
+export class ProfileEditComponent implements OnInit, OnChanges {
 
   // this is what we edit
   @Input() profile: Profile;
@@ -42,5 +43,21 @@ export class ProfileEditComponent implements OnInit {
 
   onCancel() {
     this.canceled.emit('cancelled');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // fix date of birth if this is the first time user
+    // is editing profile right after registration
+    const profileChanges: SimpleChange = changes.profile;
+    if (profileChanges != null && !profileChanges.firstChange) {
+      const strDateOfBirth = profileChanges.currentValue?.dateOfBirth;
+      if (strDateOfBirth != null) {
+        const dateOfBirth: Date = new DateUtils().convertFromString(strDateOfBirth);
+        const today = new Date();
+        if (today.getFullYear() === dateOfBirth.getFullYear()) {
+          this.profile.dateOfBirth = null;
+        }
+      }
+    }
   }
 }
