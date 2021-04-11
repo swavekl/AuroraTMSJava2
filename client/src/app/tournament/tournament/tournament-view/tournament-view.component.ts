35d372eed92eca975a422cef56a73aa1ccd5c922
Swavek <swaveklorenc@yahoo.com>
2021-04-10 21:19:11 -0500

    Added ability to launch Google maps to navigate to the venue. Call tournament director or send him email.

--- client/src/app/tournament/tournament/tournament-view/tournament-view.component.ts
+++ client/src/app/tournament/tournament/tournament-view/tournament-view.component.ts
@@ -90,4 +90,9 @@ export class TournamentViewComponent implements OnInit, OnChanges {
     };
     this.router.navigateByUrl(url, extras);
   }
+
+  showDirections() {
+    const url = 'https://www.google.com/maps/dir/?api=1&destination=Vaughan+Athletic+Center+Aurora+IL&travelmode=driving&dir_action=navigate';
+    window.open(url);
+  }
 }
