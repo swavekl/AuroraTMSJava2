85792e88350770276c5aca6a2a5d4661c8904895
Swavek <swaveklorenc@yahoo.com>
2021-12-26 23:13:42 -0600

    Filtered out matches that are already in progress in addition to those that were completed.  Added text for occupied tables.

--- client/src/app/table-usage/table-usage/table-usage-container.component.ts
+++ client/src/app/table-usage/table-usage/table-usage-container.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {combineLatest, Observable, of, Subscription} from 'rxjs';
-import {first, map} from 'rxjs/operators';
+import {combineLatest, Observable, Subscription} from 'rxjs';
+import {first, map, tap} from 'rxjs/operators';
 import {TableUsage} from '../model/table-usage.model';
 import {LinearProgressBarService} from '../../shared/linear-progress-bar/linear-progress-bar.service';
 import {TableUsageService} from '../service/table-usage.service';
@@ -13,12 +13,14 @@ import {DateUtils} from '../../shared/date-utils';
 import {TournamentEventConfigService} from '../../tournament/tournament-config/tournament-event-config.service';
 import {TournamentEvent} from '../../tournament/tournament-config/tournament-event.model';
 import {MatchCardPrinterService} from '../../matches/service/match-card-printer.service';
+import {MatchInfo} from './table-usage.component';
 
 @Component({
   selector: 'app-table-usage-container',
   template: `
     <app-table-usage [tableUsageList]="tableUsageList$ | async"
-                     [matchCards]="matchCards$ | async"
+                     [allTodaysMatchCards]="matchCards$ | async"
+                     [matchesToPlayInfos]="matchesToPlayInfos$ | async"
                      [tournamentEvents]="tournamentEvents$ | async"
                      (printMatchCards)="onPrintMatchCards($event)"
                      (startMatches)="onStartMatches($event)">
@@ -31,6 +33,8 @@ export class TableUsageContainerComponent implements OnInit, OnDestroy {
   tableUsageList$: Observable<TableUsage[]>;
 
   matchCards$: Observable<MatchCard[]>;
+
+  matchesToPlayInfos$: Observable<MatchInfo[]>;
 
   tournamentEvents$: Observable<TournamentEvent[]>;
 
@@ -82,16 +86,15 @@ export class TableUsageContainerComponent implements OnInit, OnDestroy {
       .pipe(
         first(),
         map((todaysTournaments: Tournament[]) => {
-          console.log('got todays tournaments', todaysTournaments);
           if (todaysTournaments?.length > 0) {
             const tournament: Tournament = todaysTournaments[0];
             const difference = new DateUtils().daysBetweenDates(tournament.startDate, todaysDate);
             this.tournamentDay = difference + 1;
             this.tournamentId = tournament.id;
-            console.log('tournamentid', this.tournamentId);
-            this.loadTableUsage(this.tournamentId);
             this.loadEvents(this.tournamentId);
+            this.loadTableUsage(this.tournamentId);
             this.loadMatchCards();
+            this.filterMatchCardsToPlay();
           }
         }))
       .subscribe();
@@ -100,12 +103,12 @@ export class TableUsageContainerComponent implements OnInit, OnDestroy {
 
   private loadTableUsage(tournamentId: number) {
     this.tableUsageList$ = this.tableUsageService.store.select(
-      this.tableUsageService.selectors.selectEntities)
-      .pipe(
-        map((tableUsages: TableUsage[]) => {
-          console.log('cloning table usages');
-          return JSON.parse(JSON.stringify(tableUsages));
-    }));
+      this.tableUsageService.selectors.selectEntities);
+      // .pipe(
+      //   map((tableUsages: TableUsage[]) => {
+      //     console.log('CONTAINER got table usage list ', tableUsages);
+      //     return JSON.parse(JSON.stringify(tableUsages));
+      //   }));
     const params = `tournamentId=${tournamentId}`;
     // no need to subscribe since it is subscribed in a template
     this.tableUsageService.getWithQuery(params);
@@ -119,6 +122,10 @@ export class TableUsageContainerComponent implements OnInit, OnDestroy {
   private loadEvents(tournamentId: number) {
     this.tournamentEvents$ = this.tournamentEventConfigService.store.select(
       this.tournamentEventConfigService.selectors.selectEntities);
+      // .pipe(
+      //   tap((tournamentEvents: TournamentEvent[]) => {
+      //     console.log ('CONTAINER got tournamentEvents', tournamentEvents);
+      //   }));
     // load them - they will surface via this selector
     this.tournamentEventConfigService.loadTournamentEvents(this.tournamentId);
   }
@@ -128,37 +135,75 @@ export class TableUsageContainerComponent implements OnInit, OnDestroy {
    * @private
    */
   private loadMatchCards() {
-    this.matchCards$ = this.matchCardService.store.select(this.matchCardService.selectors.selectEntities)
-      .pipe(
-        map((matchCards: MatchCard[]) => {
-          if (matchCards?.length > 0) {
-            const filteredMatchCards = [];
-            for (const matchCard of matchCards) {
-              const matchCompleted = (matchCard.playerRankings != null);
-              if (!matchCompleted) {
-                filteredMatchCards.push(matchCard);
-              } else {
-                console.log('match completed');
+    this.matchCards$ = this.matchCardService.store.select(this.matchCardService.selectors.selectEntities);
+    // .pipe(
+    //   tap((matchCards) => {
+    //     console.log('CONTAINER got match cards', matchCards);
+    //   }));
+    this.matchCardService.loadAllForTheTournamentDay(this.tournamentId, this.tournamentDay);
+  }
+
+  /**
+   * Prepares match infos for matches thar are still to be played which combines match card and event information
+   * @private
+   */
+  private filterMatchCardsToPlay() {
+    this.matchesToPlayInfos$ = combineLatest(this.tableUsageList$, this.matchCards$, this.tournamentEvents$,
+      (tableUsageList: TableUsage[], matchCards: MatchCard[], tournamentEvents: TournamentEvent[]) => {
+        const matchesToPlayInfos: MatchInfo[] = [];
+        if (tableUsageList?.length > 0 && matchCards?.length > 0 && tournamentEvents?.length > 0) {
+          // console.log('CONTAINER got all 3 lists');
+          const filteredMatchCards: MatchCard [] = [];
+          for (const matchCard of matchCards) {
+            const matchCompleted = (matchCard.playerRankings != null);
+            let isPlayedCurrently = false;
+            for (let i = 0; i < tableUsageList.length; i++) {
+              const tableUsage = tableUsageList[i];
+              if (tableUsage.matchCardFk === matchCard.id) {
+                isPlayedCurrently = true;
+                // console.log('match is currently played ' + tableUsage.matchCardFk);
+                break;
               }
             }
-            return filteredMatchCards;
+            if (!matchCompleted && !isPlayedCurrently) {
+              filteredMatchCards.push(matchCard);
+              // console.log('match is available ', matchCard.id);
+            } else if (matchCompleted) {
+              // console.log('match completed', matchCard.id);
+            }
           }
-        }));
-    this.matchCardService.loadAllForTheTournamentDay(this.tournamentId, this.tournamentDay);
+
+          // now get the tournament events combined with match cards
+          for (const matchCard of filteredMatchCards) {
+            for (const event of tournamentEvents) {
+              if (matchCard.eventFk === event.id) {
+                const matchInfo: MatchInfo = {
+                  matchCard: matchCard,
+                  tournamentEvent: event
+                };
+                matchesToPlayInfos.push(matchInfo);
+              }
+            }
+          }
+          // console.log('CONTAINER match filtering completed ---------------');
+        }
+
+        return matchesToPlayInfos;
+      });
   }
 
   public onPrintMatchCards(matchCardIds: number []) {
     for (let i = 0; i < matchCardIds.length; i++) {
       const matchCardId = matchCardIds[i];
-        this.matchCardPrinterService.download(matchCardId);
+      this.matchCardPrinterService.download(matchCardId);
     }
   }
 
   onStartMatches(tableUsages: TableUsage[]) {
-     this.tableUsageService.updateMany(tableUsages)
-       .pipe(first())
-       .subscribe((updated: TableUsage[]) => {
-         this.tableUsageService.updateManyInCache(updated);
-       });
+    this.tableUsageService.updateMany(tableUsages)
+      .pipe(first())
+      .subscribe((updated: TableUsage[]) => {
+        this.tableUsageService.updateManyInCache(updated);
+      });
   }
 }
--- client/src/app/table-usage/table-usage/table-usage.component.ts
+++ client/src/app/table-usage/table-usage/table-usage.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange, SimpleChanges} from '@angular/core';
 import {TableUsage} from '../model/table-usage.model';
 import {MatchCard} from '../../matches/model/match-card.model';
 import {TournamentEvent} from '../../tournament/tournament-config/tournament-event.model';
@@ -16,8 +16,12 @@ export class TableUsageComponent implements OnInit, OnChanges {
   @Input()
   tableUsageList: TableUsage [] = [];
 
+  // match cards and event information for match cards available to be played
   @Input()
-  matchCards: MatchCard[];
+  matchesToPlayInfos: MatchInfo [];
+
+  @Input()
+  allTodaysMatchCards: MatchCard[];
 
   @Input()
   tournamentEvents: TournamentEvent[];
@@ -28,8 +32,6 @@ export class TableUsageComponent implements OnInit, OnChanges {
   @Output()
   startMatches: EventEmitter<TableUsage[]> = new EventEmitter<TableUsage[]>();
 
-  public matchesToPlayInfos: MatchInfo [];
-
   public selectedEventId: number;
 
   public selectedMatchCardIds: number [];
@@ -37,30 +39,32 @@ export class TableUsageComponent implements OnInit, OnChanges {
   constructor() {
     this.selectedEventId = 0;
     this.selectedMatchCardIds = [];
+    this.matchesToPlayInfos = [];
+    this.tableUsageList = [];
+    this.tournamentEvents = [];
   }
 
   ngOnInit(): void {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('changes received');
-    if (this.matchCards != null && this.tournamentEvents != null) {
-      const matchesToPlay: any[] = [];
-      for (const matchCard of this.matchCards) {
-        const eventFk = matchCard.eventFk;
-        for (const tournamentEvent of this.tournamentEvents) {
-          if (tournamentEvent.id === eventFk) {
-            const matchInfo: MatchInfo = {
-              matchCard: matchCard,
-              tournamentEvent: tournamentEvent
-            };
-            matchesToPlay.push(matchInfo);
-            break;
-          }
-        }
-      }
-      this.matchesToPlayInfos = matchesToPlay;
-    }
+    // const tableUsageListChange: SimpleChange = changes.tableUsageList;
+    // if (tableUsageListChange?.currentValue != null) {
+    //   console.log('COMP got tableUsageList', tableUsageListChange.currentValue);
+    //   const tableUsageList: TableUsage[] = tableUsageListChange.currentValue;
+    // }
+    //
+    // const tournamentEventsChange: SimpleChange = changes.tournamentEvents;
+    // if (tournamentEventsChange?.currentValue != null) {
+    //   const tournamentEvents = tournamentEventsChange.currentValue;
+    //   console.log('COMP got tournamentEvents', tournamentEvents);
+    // }
+    //
+    // const matchesToPlayInfosChange: SimpleChange = changes.matchesToPlayInfos;
+    // if (matchesToPlayInfosChange?.currentValue != null) {
+    //   const matchesToPlayInfos = matchesToPlayInfosChange.currentValue;
+    //   console.log('COMP got matchesToPlayInfos', matchesToPlayInfos);
+    // }
   }
 
   onSelectMatchCard(matchCard: MatchCard) {
@@ -87,9 +91,11 @@ export class TableUsageComponent implements OnInit, OnChanges {
   }
 
   onStartEventMatches() {
+    // console.log('onStartEventMatches for event ', this.selectedEventId);
     const eventMatchCardIds: number [] = [];
-    for (let i = 0; i < this.matchCards.length; i++) {
-      const matchCard = this.matchCards[i];
+    for (let i = 0; i < this.matchesToPlayInfos.length; i++) {
+      const matchesToPlayInfo = this.matchesToPlayInfos[i];
+      const matchCard = matchesToPlayInfo.matchCard;
       if (matchCard.eventFk === this.selectedEventId && matchCard.drawType === DrawType.ROUND_ROBIN) {
         eventMatchCardIds.push(matchCard.id);
       }
@@ -102,11 +108,12 @@ export class TableUsageComponent implements OnInit, OnChanges {
   }
 
   private startSelectedMatchCards(matchCardIds: number[]) {
+    console.log('startSelectedMatchCards', matchCardIds);
     // find table numbers to update
     const startTime: Date = new Date();
     const updatedTableUsages: TableUsage [] = [];
-    for (let i = 0; i < this.matchCards.length; i++) {
-      const matchCard = this.matchCards[i];
+    for (let i = 0; i < this.matchesToPlayInfos.length; i++) {
+      const matchCard = this.matchesToPlayInfos[i].matchCard;
       for (const matchCardId of matchCardIds) {
         if (matchCardId === matchCard.id) {
           const assignedTables = matchCard.assignedTables;
@@ -142,26 +149,40 @@ export class TableUsageComponent implements OnInit, OnChanges {
   }
 
   public getMatchIdentifierText(matchCardId: number) {
-    let tooltipText = '';
-    for (let i = 0; i < this.matchesToPlayInfos.length; i++) {
-      const matchInfo = this.matchesToPlayInfos[i];
-      if (matchInfo.matchCard.id === matchCardId) {
-        const eventName = matchInfo.tournamentEvent.name;
-        const groupNumber = matchInfo.matchCard.groupNum;
-        if (matchInfo.matchCard.drawType === 'ROUND_ROBIN') {
-          tooltipText = `${eventName} R.R. Group ${groupNumber}`;
-        } else {
-          const roundName = this.getRoundShortName(matchInfo.matchCard.round);
-          tooltipText = `${eventName} ${roundName} M ${groupNumber}`;
+    // console.log('getMatchIdentifierText ', matchCardId);
+    let matchIdentifierText = '';
+    if (this.allTodaysMatchCards && matchCardId !== 0) {
+      for (let i = 0; i < this.allTodaysMatchCards.length; i++) {
+        const matchCard = this.allTodaysMatchCards[i];
+        if (matchCard.id === matchCardId) {
+          const eventName = this.getEventName(matchCard.eventFk);
+          const groupNumber = matchCard.groupNum;
+          if (matchCard.drawType === 'ROUND_ROBIN') {
+            matchIdentifierText = `${eventName} R.R. Group ${groupNumber}`;
+          } else {
+            const roundName = this.getRoundShortName(matchCard.round);
+            matchIdentifierText = `${eventName} ${roundName} M ${groupNumber}`;
+          }
+          break;
         }
       }
+      // console.log(matchCardId + ' -> ' + matchIdentifierText);
     }
-
-    return tooltipText;
+    return matchIdentifierText;
   }
 
   isSelectedMatchCard(matchCard: MatchCard) {
     return this.selectedMatchCardIds.includes(matchCard.id);
+  }
+
+  private getEventName(eventFk: number) {
+    for (let i = 0; i < this.tournamentEvents.length; i++) {
+      const tournamentEvent = this.tournamentEvents[i];
+      if (tournamentEvent.id === eventFk) {
+        return tournamentEvent.name;
+      }
+    }
+    return '';
   }
 }
 
