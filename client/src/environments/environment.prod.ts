ade0e749756180bf43b72853fdddbdab81b5b93e
Swavek <swaveklorenc@yahoo.com>
2023-01-21 14:04:02 -0600

    Made the application work on public internet.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // myAppUrl: 'https://localhost:4200',
-  // baseServer: 'gateway-pc:8080'
-  myAppUrl: 'https://gateway-pc:4200',
-  baseServer: 'gateway-pc:8443'
+  // myAppUrl: 'https://gateway-pc:4200',
+  // baseServer: 'gateway-pc:8443'
+  myAppUrl: 'https://ttaurora.com:443',
+  baseServer: 'ttaurora.com:443'
 };
