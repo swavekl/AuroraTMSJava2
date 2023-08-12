2bd827f6ecf6de0b995f5439890f0d14b25322e8
Swavek <swaveklorenc@yahoo.com>
2023-08-12 16:44:07 -0500

    Fix today service

--- client/src/app/shared/today.service.ts
+++ client/src/app/shared/today.service.ts
@@ -53,6 +53,7 @@ export class TodayService {
     // const utcMoment = moment([2023, 1, 25, 0, 0, 0]).utc();
     // const utcMoment = moment([2022, 11, 20, 0, 0, 0]).utc();
     // Aurora Cup Real - RR & Single Elimination
+    // const utcMoment = moment([2023, 0, 15, 0, 0, 0]);
     // const utcMoment = moment([2023, 0, 14, 0, 0, 0]);
     // const utcMoment = moment([2023, 0, 13, 0, 0, 0]);
     // Prisco Mini - Single Elimination
