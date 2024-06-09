d6776e0e42c32b8593eb241b0f30421216fe2abf
Swavek <swaveklorenc@yahoo.com>
2024-06-09 14:26:54 -0500

    Added tests for singles matches

--- client/src/app/shared/serve-order/order-of-serving-calculator.spec.ts
+++ client/src/app/shared/serve-order/order-of-serving-calculator.spec.ts
@@ -1,18 +1,19 @@
 import {OrderOfServingCalculator} from './order-of-serving-calculator';
 
 describe('OrderOfServingCalculator', () => {
-  const calculator = new OrderOfServingCalculator(5, true);
+  // doubles
+  let calculator = new OrderOfServingCalculator(5, true);
   calculator.recordServerAndReceiver('A', 'X');
-  it('initial server A', () => {
+  let lastDoublesGame = 1;
+  it('Doubles initial server A', () => {
     expect(calculator.getServer()).toMatch('A');
   });
-  it('initial receiver X', () => {
+  it('Doubles initial receiver X', () => {
     expect(calculator.getReceiver()).toMatch('X');
   });
 
-  let lastGame = 1;
   test.each([
-    // {game: 1, pointsSideA: 0, pointsSideB: 0, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 0, pointsSideB: 0, expectedServer: "A", expectedReceiver: "X"},
     {game: 1, pointsSideA: 0, pointsSideB: 1, expectedServer: "A", expectedReceiver: "X"},
     {game: 1, pointsSideA: 1, pointsSideB: 1, expectedServer: "X", expectedReceiver: "B"},
     {game: 1, pointsSideA: 2, pointsSideB: 1, expectedServer: "X", expectedReceiver: "B"},
@@ -50,6 +51,7 @@ describe('OrderOfServingCalculator', () => {
     {game: 2, pointsSideA: 8, pointsSideB: 8, expectedServer: "Y", expectedReceiver: "B"},
     {game: 2, pointsSideA: 9, pointsSideB: 8, expectedServer: "Y", expectedReceiver: "B"},
     {game: 2, pointsSideA: 9, pointsSideB: 9, expectedServer: "B", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 9, pointsSideB: 10, expectedServer: "B", expectedReceiver: "X"},
     {game: 2, pointsSideA: 10, pointsSideB: 10, expectedServer: "X", expectedReceiver: "A"},
     {game: 2, pointsSideA: 11, pointsSideB: 10, expectedServer: "A", expectedReceiver: "Y"},
     {game: 2, pointsSideA: 11, pointsSideB: 11, expectedServer: "Y", expectedReceiver: "B"},
@@ -113,32 +115,33 @@ describe('OrderOfServingCalculator', () => {
     {game: 5, pointsSideA: 9, pointsSideB: 4, expectedServer: "B", expectedReceiver: "X"},
     {game: 5, pointsSideA: 10, pointsSideB: 4, expectedServer: "X", expectedReceiver: "A"},
     {game: 5, pointsSideA: 10, pointsSideB: 5, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 10, pointsSideB: 6, expectedServer: "A", expectedReceiver: "Y"},
 
 
-  ])('Game $game at ($pointsSideA, $pointsSideB) -> expected server $expectedServer, expected receiver $expectedReceiver',
+  ])('Doubles game $game at ($pointsSideA, $pointsSideB) -> expected server $expectedServer, expected receiver $expectedReceiver',
     ({game, pointsSideA, pointsSideB, expectedServer, expectedReceiver}) => {
-      if (lastGame != game) {
-        lastGame = game;
+      if (lastDoublesGame != game) {
+        lastDoublesGame = game;
         const nextGameIndex = calculator.startNextGame();
         let nextReceiver = null;
         switch (nextGameIndex) {
           case 1:
-            nextReceiver = calculator.determineNextReceiverFromServer('Y');
+            nextReceiver = calculator.determineNextReceiver('Y');
             expect(nextReceiver).toMatch('B');
             calculator.recordServerAndReceiver('Y', nextReceiver);
             break;
           case 2:
-            nextReceiver = calculator.determineNextReceiverFromServer('B');
+            nextReceiver = calculator.determineNextReceiver('B');
             expect(nextReceiver).toMatch('Y');
             calculator.recordServerAndReceiver('B', nextReceiver);
             break;
           case 3:
-            nextReceiver = calculator.determineNextReceiverFromServer('X');
+            nextReceiver = calculator.determineNextReceiver('X');
             expect(nextReceiver).toMatch('A');
             calculator.recordServerAndReceiver('X', nextReceiver);
             break;
           case 4:
-            nextReceiver = calculator.determineNextReceiverFromServer('B');
+            nextReceiver = calculator.determineNextReceiver('B');
             expect(nextReceiver).toMatch('Y');
             calculator.recordServerAndReceiver('B', nextReceiver);
             break;
@@ -148,5 +151,156 @@ describe('OrderOfServingCalculator', () => {
       calculator.determineNextServerAndReceiver(pointsSideA, pointsSideB);
     expect(calculator.getServer()).toMatch(expectedServer);
     expect(calculator.getReceiver()).toMatch(expectedReceiver);
+  });
+
+
+  let singlesCalculator = new OrderOfServingCalculator(5, false);
+  singlesCalculator.recordServerAndReceiver('A', 'X');
+  let lastSinglesGame = 1;
+  it('Singles initial server A', () => {
+    expect(singlesCalculator.getServer()).toMatch('A');
+  });
+  it('Singles initial receiver X', () => {
+    expect(singlesCalculator.getReceiver()).toMatch('X');
+  });
+
+  test.each([
+    {game: 1, pointsSideA: 0, pointsSideB: 0, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 0, pointsSideB: 1, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 1, pointsSideB: 1, expectedServer: "X", expectedReceiver: "A"},
+    {game: 1, pointsSideA: 2, pointsSideB: 1, expectedServer: "X", expectedReceiver: "A"},
+    {game: 1, pointsSideA: 3, pointsSideB: 1, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 3, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 3, pointsSideB: 3, expectedServer: "X", expectedReceiver: "A"},
+    {game: 1, pointsSideA: 3, pointsSideB: 4, expectedServer: "X", expectedReceiver: "A"},
+    {game: 1, pointsSideA: 3, pointsSideB: 5, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 3, pointsSideB: 6, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 4, pointsSideB: 6, expectedServer: "X", expectedReceiver: "A"},
+    {game: 1, pointsSideA: 5, pointsSideB: 6, expectedServer: "X", expectedReceiver: "A"},
+    {game: 1, pointsSideA: 6, pointsSideB: 6, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 6, pointsSideB: 7, expectedServer: "A", expectedReceiver: "X"},
+    {game: 1, pointsSideA: 6, pointsSideB: 8, expectedServer: "X", expectedReceiver: "A"},
+    {game: 1, pointsSideA: 6, pointsSideB: 9, expectedServer: "X", expectedReceiver: "A"},
+    {game: 1, pointsSideA: 6, pointsSideB: 10, expectedServer: "A", expectedReceiver: "X"},
+    // 0 : 1
+    // game 2
+    {game: 2, pointsSideA: 0, pointsSideB: 0, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 1, pointsSideB: 0, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 2, pointsSideB: 0, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 3, pointsSideB: 0, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 3, pointsSideB: 1, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 3, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 3, pointsSideB: 3, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 3, pointsSideB: 4, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 4, pointsSideB: 4, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 5, pointsSideB: 4, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 5, pointsSideB: 5, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 6, pointsSideB: 5, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 7, pointsSideB: 5, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 7, pointsSideB: 6, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 8, pointsSideB: 6, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 8, pointsSideB: 7, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 8, pointsSideB: 8, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 9, pointsSideB: 8, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 9, pointsSideB: 9, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 9, pointsSideB: 10, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 10, pointsSideB: 10, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 11, pointsSideB: 10, expectedServer: "A", expectedReceiver: "X"},
+    {game: 2, pointsSideA: 11, pointsSideB: 11, expectedServer: "X", expectedReceiver: "A"},
+    {game: 2, pointsSideA: 12, pointsSideB: 11, expectedServer: "A", expectedReceiver: "X"},
+    // 1 : 1
+    // game 3
+    {game: 3, pointsSideA: 0, pointsSideB: 0, expectedServer: "A", expectedReceiver: "X"},
+    {game: 3, pointsSideA: 0, pointsSideB: 1, expectedServer: "A", expectedReceiver: "X"},
+    {game: 3, pointsSideA: 1, pointsSideB: 1, expectedServer: "X", expectedReceiver: "A"},
+    {game: 3, pointsSideA: 1, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 3, pointsSideA: 2, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 3, pointsSideA: 3, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 3, pointsSideA: 4, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 3, pointsSideA: 5, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 3, pointsSideA: 6, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 3, pointsSideA: 7, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 3, pointsSideA: 7, pointsSideB: 3, expectedServer: "X", expectedReceiver: "A"},
+    {game: 3, pointsSideA: 8, pointsSideB: 3, expectedServer: "X", expectedReceiver: "A"},
+    {game: 3, pointsSideA: 8, pointsSideB: 4, expectedServer: "A", expectedReceiver: "X"},
+    {game: 3, pointsSideA: 9, pointsSideB: 4, expectedServer: "A", expectedReceiver: "X"},
+    {game: 3, pointsSideA: 10, pointsSideB: 4, expectedServer: "X", expectedReceiver: "A"},
+    {game: 3, pointsSideA: 10, pointsSideB: 5, expectedServer: "X", expectedReceiver: "A"},
+    // 2 : 1
+    // game 3
+    {game: 4, pointsSideA: 0, pointsSideB: 0, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 0, pointsSideB: 1, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 1, pointsSideB: 1, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 1, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 2, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 3, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 4, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 5, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 6, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 7, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 7, pointsSideB: 3, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 8, pointsSideB: 3, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 8, pointsSideB: 4, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 8, pointsSideB: 5, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 8, pointsSideB: 6, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 8, pointsSideB: 7, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 8, pointsSideB: 8, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 8, pointsSideB: 9, expectedServer: "X", expectedReceiver: "A"},
+    {game: 4, pointsSideA: 8, pointsSideB: 10, expectedServer: "A", expectedReceiver: "X"},
+    {game: 4, pointsSideA: 9, pointsSideB: 10, expectedServer: "A", expectedReceiver: "X"},
+    // 2 : 2
+    // game 5
+    {game: 5, pointsSideA: 0, pointsSideB: 0, expectedServer: "A", expectedReceiver: "X"},
+    {game: 5, pointsSideA: 0, pointsSideB: 1, expectedServer: "A", expectedReceiver: "X"},
+    {game: 5, pointsSideA: 1, pointsSideB: 1, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 1, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 2, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 5, pointsSideA: 3, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 5, pointsSideA: 4, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 5, pointsSideB: 2, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 6, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 5, pointsSideA: 7, pointsSideB: 2, expectedServer: "A", expectedReceiver: "X"},
+    {game: 5, pointsSideA: 7, pointsSideB: 3, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 8, pointsSideB: 3, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 8, pointsSideB: 4, expectedServer: "A", expectedReceiver: "X"},
+    {game: 5, pointsSideA: 9, pointsSideB: 4, expectedServer: "A", expectedReceiver: "X"},
+    {game: 5, pointsSideA: 10, pointsSideB: 4, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 10, pointsSideB: 5, expectedServer: "X", expectedReceiver: "A"},
+    {game: 5, pointsSideA: 10, pointsSideB: 6, expectedServer: "A", expectedReceiver: "X"},
+
+
+  ])('Singles game $game at ($pointsSideA, $pointsSideB) -> expected server $expectedServer, expected receiver $expectedReceiver',
+    ({game, pointsSideA, pointsSideB, expectedServer, expectedReceiver}) => {
+    if (lastSinglesGame != game) {
+      lastSinglesGame = game;
+        const nextGameIndex = singlesCalculator.startNextGame();
+        let nextReceiver = null;
+        switch (nextGameIndex) {
+          case 1:
+            nextReceiver = singlesCalculator.determineNextReceiver('X');
+            expect(nextReceiver).toMatch('A');
+            singlesCalculator.recordServerAndReceiver('X', nextReceiver);
+            break;
+          case 2:
+            nextReceiver = singlesCalculator.determineNextReceiver('A');
+            expect(nextReceiver).toMatch('X');
+            singlesCalculator.recordServerAndReceiver('A', nextReceiver);
+            break;
+          case 3:
+            nextReceiver = singlesCalculator.determineNextReceiver('X');
+            expect(nextReceiver).toMatch('A');
+            singlesCalculator.recordServerAndReceiver('X', nextReceiver);
+            break;
+          case 4:
+            nextReceiver = singlesCalculator.determineNextReceiver('A');
+            expect(nextReceiver).toMatch('X');
+            singlesCalculator.recordServerAndReceiver('A', nextReceiver);
+            break;
+        }
+      }
+
+      singlesCalculator.determineNextServerAndReceiver(pointsSideA, pointsSideB);
+    expect(singlesCalculator.getServer()).toMatch(expectedServer);
+    expect(singlesCalculator.getReceiver()).toMatch(expectedReceiver);
   });
 });
--- client/src/app/shared/serve-order/order-of-serving-calculator.ts
+++ client/src/app/shared/serve-order/order-of-serving-calculator.ts
@@ -3,6 +3,7 @@
  */
 export class OrderOfServingCalculator {
 
+  // if true it is a doubles event
   doubles: boolean = false;
 
   // we move in these arrays in circular fashion
@@ -12,10 +13,17 @@ export class OrderOfServingCalculator {
   // trivial case for singles A -> X, X -> A
   singlesOrderOfPlayers: string [] = ['A', 'X'];
 
+  // if true the current game serving order is determined by cycling forward in orderOfPlayers array
+  // if false backwards
+  cyclingForward: boolean = true;
+
+  // current game index - 0 based
   currentGame: number = 0;
 
+  // array of servers and receivers for given games e.g. A,X means A serving X
   gameServerAndReceiver: string [] = [];
 
+  // current server and receiver letters
   // letters designating players e.g. A X Y or B
   currentServer: string;
   currentReceiver: string;
@@ -29,39 +37,63 @@ export class OrderOfServingCalculator {
     return (this.doubles) ? this.doublesOrderOfPlayers : this.singlesOrderOfPlayers;
   }
 
-  startNextGame(): number {
+  /**
+   * advances to the next game
+   */
+  public startNextGame(): number {
     return ++this.currentGame;
   }
 
-  recordServerAndReceiver(serverLetter: string, receiverLetter: string): void  {
+  /**
+   * Records server and receiver of the current game
+   * @param serverLetter
+   * @param receiverLetter
+   */
+  public recordServerAndReceiver(serverLetter: string, receiverLetter: string): void {
     this.gameServerAndReceiver[this.currentGame] = `${serverLetter},${receiverLetter}`;
     this.currentServer = serverLetter;
     this.currentReceiver = receiverLetter;
+    this.cyclingForward = this.isCyclingForward(this.currentServer, this.currentReceiver);
   }
 
-  public determineNextServerAndReceiver(pointsSideA: number, pointsSideB: number) {
-    const orderOfPlayers = this.getOrderOfPlayers()
+  /**
+   * Finds out if it is cyclying forward or backwards based on the passed in server and receiver
+   * @param currentServer
+   * @param currentReceiver
+   */
+  private isCyclingForward(currentServer: string, currentReceiver: string): boolean {
+    const orderOfPlayers = this.getOrderOfPlayers();
     // determine direction of cycling through A X B Y order or A X
+    let idxServer = orderOfPlayers.indexOf(currentServer);
+    let idxReceiver = orderOfPlayers.indexOf(currentReceiver);
+    return (idxServer + 1 === idxReceiver) ||
+      (idxServer === (orderOfPlayers.length - 1) && idxReceiver === 0);
+  }
+
+  /**
+   *
+   * @param pointsSideA
+   * @param pointsSideB
+   */
+  public determineNextServerAndReceiver(pointsSideA: number, pointsSideB: number) {
+    const orderOfPlayers = this.getOrderOfPlayers();
     let idxServer = orderOfPlayers.indexOf(this.currentServer);
     let idxReceiver = orderOfPlayers.indexOf(this.currentReceiver);
-    let cycleForward = (idxServer + 1 === idxReceiver) ||
-      (idxServer === (orderOfPlayers.length - 1) && idxReceiver === 0);
 
     const totalPoints = pointsSideA + pointsSideB;
     const divisor = (pointsSideA >= 10 && pointsSideB >= 10) ? 1 : 2;
     const remainder = totalPoints % divisor;
+    // figure out if server and receiver changes (every 2 points or at 10 : 10)
     const changeServer = (remainder === 0) && (totalPoints > 0);
-    console.log('changeServer', changeServer);
     let changeReceiver: boolean = changeServer;
     if (changeServer) {
-      if (cycleForward) {
+      if (this.cyclingForward) {
         idxServer++;
         idxServer = (idxServer === orderOfPlayers.length) ? 0 : idxServer;
       } else {
         idxServer--;
         idxServer = (idxServer < 0) ? orderOfPlayers.length - 1 : idxServer;
       }
-      console.log('idxServer', idxServer);
       this.currentServer = orderOfPlayers[idxServer];
     }
 
@@ -70,47 +102,50 @@ export class OrderOfServingCalculator {
       // if this is the last game
       if (this.currentGame === (this.gameServerAndReceiver.length - 1))
         // one of the teams won 5 points
-        if ((pointsSideA === 5 && pointsSideB !== 5) ||
-            (pointsSideA !== 5 && pointsSideB === 5)) {
-        changeReceiver = true;
-          cycleForward = !cycleForward;
-      }
-
-      // // change cyclind direction for the rest of the last deciding game
-      // if (pointsSideA >= 5 || pointsSideB >= 5) {
-      // }
+        if ((pointsSideA === 5 && pointsSideB < 5) ||
+          (pointsSideA < 5 && pointsSideB === 5)) {
+          changeReceiver = true;
+          this.cyclingForward = !this.cyclingForward;
+          // adjust index so calculation below is properly cycling to the other receiver
+          if (this.cyclingForward) {
+            idxReceiver++;
+          } else {
+            idxReceiver--;
+          }
+        }
     }
 
     if (changeReceiver) {
-      if (cycleForward) {
+      if (this.cyclingForward) {
         idxReceiver++;
-        idxReceiver = (idxReceiver === orderOfPlayers.length) ? 0 : idxReceiver
+        idxReceiver = (idxReceiver === orderOfPlayers.length) ? 0 : idxReceiver;
       } else {
         idxReceiver--;
         idxReceiver = (idxReceiver < 0) ? orderOfPlayers.length - 1 : idxReceiver;
       }
-      console.log('idxReceiver', idxReceiver);
       this.currentReceiver = orderOfPlayers[idxReceiver];
     }
   }
 
-  public determineNextReceiverFromServer(selectedServer: string) {
+  /**
+   * Determines the next receiver given selected server based on the previous game's serving order
+   * @param selectedServer
+   */
+  public determineNextReceiver(selectedServer: string) {
+    // get serving direction of the previous game
     const previousGame = Math.max(0, this.currentGame - 1);
     const currentGameServerAndReceiver = this.gameServerAndReceiver[previousGame];
-    const playerLetters: string [] = currentGameServerAndReceiver.split(",");
+    const playerLetters: string [] = currentGameServerAndReceiver.split(',');
     const serverPlayer = playerLetters[0];
     const receiverPlayer = playerLetters[1];
-
-    const orderOfPlayers = this.getOrderOfPlayers()
-    // determine direction of cycling through A X B Y order or A X
-    let idxServer = orderOfPlayers.indexOf(serverPlayer);
-    let idxReceiver = orderOfPlayers.indexOf(receiverPlayer);
-    let cycleForward = (idxServer + 1 === idxReceiver) ||
-      (idxServer === (orderOfPlayers.length - 1) && idxReceiver === 0);
+    const wasCyclingForward = this.isCyclingForward(serverPlayer, receiverPlayer);
 
     // reverse the direction
-    cycleForward = !cycleForward;
+    const cycleForward = !wasCyclingForward;
+
+    const orderOfPlayers = this.getOrderOfPlayers();
     let idxNewServer = orderOfPlayers.indexOf(selectedServer);
+    let idxReceiver = 0;
     if (cycleForward) {
       idxReceiver = idxNewServer + 1;
       idxReceiver = (idxReceiver === orderOfPlayers.length) ? 0 : idxReceiver;
@@ -132,7 +167,7 @@ export class OrderOfServingCalculator {
   /**
    * prepare string to record in database
    */
-  public toJson (): string {
+  public toJson(): string {
     return JSON.stringify({
       doubles: this.doubles,
       currentGame: this.currentGame,
