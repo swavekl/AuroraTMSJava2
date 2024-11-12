cc5bfc73eadba60f669c7b895b478d5c1a8f32c1
Swavek <swaveklorenc@yahoo.com>
2024-11-11 20:20:22 -1000

    Fixed build issue for production (revert to Angula 17).  Fixed day of the week for event pipe.

--- client/src/app/shared/pipes/event-day-pipe.pipe.ts
+++ client/src/app/shared/pipes/event-day-pipe.pipe.ts
@@ -9,9 +9,9 @@ export class EventDayPipePipe implements PipeTransform {
   transform(eventDay: number, tournamentStartDate: any, ...args: unknown[]): string {
     let strEventDayName = '';
     if (tournamentStartDate != null) {
-      const localMoment = moment([tournamentStartDate.getFullYear(), tournamentStartDate.getMonth(), tournamentStartDate.getDate(), 0, 0, 0]).utc();
-      // event days are 1 based - 1st day will be 1, 2nd day 2, etc.
+      const localMoment = moment(tournamentStartDate).utc();
       localMoment.add(eventDay - 1, 'day');
+      // event days are 1 based - 1st day will be 1, 2nd day 2, etc.
       strEventDayName = localMoment.format('dddd');  // format date as day of the week
     }
     return strEventDayName;
