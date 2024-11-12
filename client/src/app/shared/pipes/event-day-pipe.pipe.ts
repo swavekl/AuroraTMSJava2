e02beb06ee6b92da2cb5d9dd9c39c98f5beae8eb
Swavek <swaveklorenc@yahoo.com>
2024-11-11 19:20:27 -1000

    Used the same date formatter on config list.

--- client/src/app/shared/pipes/event-day-pipe.pipe.ts
+++ client/src/app/shared/pipes/event-day-pipe.pipe.ts
@@ -1,12 +1,10 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import moment, {Moment} from 'moment';
+import moment from 'moment';
 
 @Pipe({
   name: 'eventDay'
 })
 export class EventDayPipePipe implements PipeTransform {
-
-  readonly TIME_PORTION: string = 'T00:00:00.000+00:00';
 
   transform(eventDay: number, tournamentStartDate: any, ...args: unknown[]): string {
     let strEventDayName = '';
