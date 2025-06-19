12c5e72d9d62b66e86db00302f5260798fc39105
Swavek <swaveklorenc@yahoo.com>
2025-06-19 14:45:19 -0500

    Corrected email campaign list screen to show total campaigns correctly. Added ability to sort.

--- server/src/main/java/com/auroratms/email/campaign/EmailCampaignService.java
+++ server/src/main/java/com/auroratms/email/campaign/EmailCampaignService.java
@@ -11,17 +11,18 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.acls.domain.BasePermission;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @CacheConfig(cacheNames = {"emailcampaign"})
@@ -45,6 +46,7 @@ public class EmailCampaignService {
                             UserRolesHelper.isTournamentDirector() ? UserRoles.TournamentDirectors :
                                     UserRoles.Everyone;
             String containsName = (StringUtils.isNotEmpty(nameLike)) ? "%" + nameLike + "%" : "%";
+            pageable = getPageable(pageable);
             Page<EmailCampaignEntity> pageOfEntities = this.repository.findAllCustom(
                     containsName, currentUserName, authority, BasePermission.WRITE.getMask(), pageable);
 
@@ -55,11 +57,39 @@ public class EmailCampaignService {
                 EmailCampaign EmailCampaign = new EmailCampaign().convertFromEntity(entity);
                 EmailCampaignList.add(EmailCampaign);
             }
-            return new PageImpl<>(EmailCampaignList);
+            return new PageImpl<>(EmailCampaignList, pageable, pageOfEntities.getTotalElements());
         } catch (Exception e) {
             log.error("Error listing email campaigns", e);
             throw e;
         }
+    }
+
+    /**
+     * Converts camel case property names to snake column names because we are using custom query
+     * @param pageable input
+     * @return new pageable with snake style column names
+     */
+    private Pageable getPageable(Pageable pageable) {
+        if (pageable != null) {
+            List<Sort.Order> orderList = pageable.getSort().stream().map(order -> {
+                String property = order.getProperty();
+                switch (property) {
+                    case "dateSent":
+                        property = "date_sent";
+                        break;
+                    case "tournamentName":
+                        property = "tournament_name";
+                        break;
+                    case "emailsCount":
+                        property = "emails_count";
+                        break;
+                }
+
+                return new Sort.Order(order.getDirection(), property);
+            }).collect(Collectors.toList());
+            pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(orderList));
+        }
+        return pageable;
     }
 
     @Cacheable(key = "#id")
